ac58e795fc5c657892582ea7f1cc65db35a03e52
yyjinlong <914580714@qq.com>
2015-11-18 08:57:08 +0800

    glance

---
+++ admin-openrc.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#
+# Copyright @2015 jinlong.yang.
+#
+# Author: jinlong.yang <yangjinlong>
+#
+
+export OS_PROJECT_DOMAIN_ID=default
+export OS_USER_DOMAIN_ID=default
+export OS_PROJECT_NAME=admin
+export OS_TENANT_NAME=admin
+export OS_USERNAME=admin
+export OS_PASSWORD=5564155
+export OS_AUTH_URL=http://localhost:35357/v3
+export OS_REGION_NAME=RegionOne
---
+++ demo-openrc.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#
+# Copyright @2015 jinlong.yang.
+#
+# Author: jinlong.yang <yangjinlong>
+#
+
+export OS_PROJECT_DOMAIN_ID=default
+export OS_USER_DOMAIN_ID=default
+export OS_PROJECT_NAME=demo
+export OS_TENANT_NAME=demo
+export OS_USERNAME=demo
+export OS_PASSWORD=5564155
+export OS_AUTH_URL=http://localhost:5000/v3
+export OS_REGION_NAME=RegionOne
